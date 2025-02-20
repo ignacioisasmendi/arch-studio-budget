39486635cb168968a87ff84d20e361366b9d2fbd
ignacioisasmendi <isasmendi.ignacio@gmail.com>
2025-02-20 16:08:17 -0300

    feat: page title

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,14 @@
 import type React from "react"
 import "@/styles/globals.css"
 import { Inter } from "next/font/google"
+import { Metadata } from "next"
 
 const inter = Inter({ subsets: ["latin"] })
+
+export const metadata: Metadata = {
+  title: "Ted Quinton Architecture Budget", // Change this dynamically in specific pages
+  description: "Budget",
+}
 
 export default function RootLayout({
   children,
